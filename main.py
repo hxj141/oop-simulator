c55587280f7b3cea1ae1c792cc5ef5388b00022f
HJ <hxj141@psu.edu>
2023-03-20 23:31:25 -0400

    Completed dispatch

--- main.py
+++ main.py
@@ -5,12 +5,13 @@ fetchIndex = 1 # Currently fetched index
 committedInsts = 0 # Currently commited instructions
 
 # Initialize clocks one below their initial time
-clock = {'fe':-1, 'de':0, 're':1, 'di':2, 'is':3, 'wb',4, 'co':5}
+clock = {'fe':-1, 'de':0, 're':1, 'di':2, 'is':3, 'wb':4, 'co':5}
 
     # Queues
 fetchDecode_queue = []
 decodeRename_queue = []
 renameDispatch_queue = []
+issue_queue = []
 
     # Prepare lines for processing
 with open("input/"+sys.argv[1]) as f: # import in file based on execution argument
@@ -28,15 +29,20 @@ instCount = len(data) - 1 # Total instructions
 preg_count = data[0][0] # Number of physical registers in system
 issue_width = data[0][1] # Width of pipeline
 
-    # Initalize free list and map table, first 32 elements are mapped
+    # Initalize free list, ready table and map table, first 32 elements are mapped
 overwrite_dict = {} # Not sure if correct, new:old format
+map_table = {}
+ready_table = {}
 free_list = [] 
 for x in range(32,preg_count):
-    free_list.append(x)
-map_table = {}
+    free_list.append('p'+str(x))
 for x in range(0,32):
     map_table[x] = 'p'+str(x)
-
+for x in range(0,preg_count):
+    if x < 32:
+        ready_table['p'+str(x)] = True
+    else:
+        ready_table['p'+str(x)] = False
 # Fetch stage
 def fetch(fetchIndex): 
     bundle = data[fetchIndex:1+(issue_width*fetchIndex)]
@@ -71,35 +77,39 @@ def rename():
             # Start with the reads
             for r in range(0,len(inst['read'])):
                 inst['read'][r] = map_table[inst['read'][r]]
-            # Pull from free list, send overwritten to ROB, 
+            # Pull from free list, track overwritten reg, update map table,  update inst dict
             for w in range(0,len(inst['write'])):
                 new_reg = free_list.pop(0)
-                overwrite_dict[new_reg] = inst['write'][w] # Not sure if to send to ROB here, might need to account for blank mapping
+                overwrite_dict[new_reg] = 'p' + str(inst['write'][w]) # Not sure if to send to ROB here, might need to account for blank mapping
                 map_table[inst['write'][w]] = new_reg
                 inst['write'][w] = map_table[inst['write'][w]]
-            # Compare regs in current inst to others in bundle to identify dependencies
-            for i in decodeRename_queue:
-                cmp_inst = i
-                war_check = set(cmp_inst['write']).intersection(inst['read'])
-                waw_check = set(cmp_inst['write']).intersection(inst['write'])
-                # WAR/WAW require hardware renaming
-                if war_check:
-                    
-                if waw_check:
-                    # look in free list, update map table, update inst dict, update free list
-                for r in inst['write']:
-                      
             renameDispatch_queue.append(inst)
     else:
         pass
     clock['re'] = clock['re'] + 1
 def dispatch():
-    inst = renameDispatch_queue.pop(inst)
+    for n in range(0,issue_width):
+        inst = renameDispatch_queue.pop(0)
+        # Add an aging property to the instruction
+        if not issue_queue:
+            inst['age'] = 0
+        else:
+            inst['age'] = issue_queue[-1]['age'] + 1
+        # Update ready table for destination entries
+        for w in range(0,len(inst['write'])):
+            ready_table[w] = False
+        issue_queue.append(inst)
     clock['di'] = clock['di'] + 1
-
+def issue():
+    pass
 #while (committedInsts < instCount):
 for t in range(0,1):
     fetch(fetchIndex)
     decode()
-    print(decodeRename_queue)
     rename()
+    dispatch()
+#    print(ready_table)
+#    print(issue_queue)
+#    print(clock)
+#    print(map_table)
+#    print(overwrite_dict)
