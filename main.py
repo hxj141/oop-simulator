5d74b6a32d41bd2f82cda8e3bb050e97e6b55c93
HJ <hxj141@psu.edu>
2023-03-21 19:21:50 -0400

    issue

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ import sys
 # Initialize parameters
 fetchIndex = 1 # Currently fetched index
 committedInsts = 0 # Currently commited instructions
-
+iq_size = 256
+rob_size = 256
 # Initialize clocks one below their initial time
 clock = {'fe':-1, 'de':0, 're':1, 'di':2, 'is':3, 'wb':4, 'co':5}
 
@@ -11,6 +12,7 @@ clock = {'fe':-1, 'de':0, 're':1, 'di':2, 'is':3, 'wb':4, 'co':5}
 fetchDecode_queue = []
 decodeRename_queue = []
 renameDispatch_queue = []
+issueWB_queue = []
 issue_queue = []
 
     # Prepare lines for processing
@@ -48,7 +50,7 @@ def fetch(fetchIndex):
     bundle = data[fetchIndex:1+(issue_width*fetchIndex)]
     for n in bundle:
         fetchDecode_queue.append(n)
-    clock['fe'] = clock['fe'] + 1
+    clock['fe'] += 1
 # Decode stage
 def decode():
     # Move to new queue
@@ -68,7 +70,7 @@ def decode():
             case 'I':
                 instDict = {'inst':'I', 'read':[inst[1]], 'write':[inst[2]]}
                 decodeRename_queue.append(instDict)
-    clock['de'] = clock['de'] + 1
+    clock['de'] += 1
 def rename():
     # Only perform if enough insts to continue, else stall
     if (len(free_list) >=  issue_width):
@@ -84,9 +86,7 @@ def rename():
                 map_table[inst['write'][w]] = new_reg
                 inst['write'][w] = map_table[inst['write'][w]]
             renameDispatch_queue.append(inst)
-    else:
-        pass
-    clock['re'] = clock['re'] + 1
+    clock['re'] += 1
 def dispatch():
     for n in range(0,issue_width):
         inst = renameDispatch_queue.pop(0)
@@ -99,17 +99,41 @@ def dispatch():
         for w in range(0,len(inst['write'])):
             ready_table[w] = False
         issue_queue.append(inst)
-    clock['di'] = clock['di'] + 1
-def issue():
-    pass
+    clock['di'] += 1
+def issue():  
+    global issue_queue
+    if (len(issue_queue) >= issue_width):
+        # Pre-sort by age in ascending order so oldest are always prioritized
+        issue_queue = sorted(issue_queue, key=lambda d: d['age'])
+        selected = 0 
+        for n in range(0,len(issue_queue)):
+            if (selected >= issue_width): # Stop when you've selected N instructions
+                break
+            inst = issue_queue[n-selected]
+            #Check if dependent instructions are ready, if so, select and place into WB
+            rcheck_flag = 1
+            for r in inst['read']:
+                if ready_table[r] == False:
+                    rcheck_flag = 0
+            if rcheck_flag:
+                issueWB_queue.append(issue_queue.pop(n-selected))
+                selected += 1
+        # After instructions have been selected, wake up all the dependent ones
+        for s in range(0,selected):
+            dep_list = issueWB_queue[-1-s]['write'] 
+            for d in dep_list:
+                ready_table[d] = True
+    clock['is'] += 1
 #while (committedInsts < instCount):
 for t in range(0,1):
     fetch(fetchIndex)
     decode()
     rename()
     dispatch()
-#    print(ready_table)
+    issue()
+    print(ready_table)
 #    print(issue_queue)
+#    print(issueWB_queue)
 #    print(clock)
 #    print(map_table)
 #    print(overwrite_dict)
