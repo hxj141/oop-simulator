a089f8d1d00baf4a6513b1da8db5e93dd0254164
HJ <hxj141@psu.edu>
2023-03-22 17:06:53 -0400

    Screwy clock

--- main.py
+++ main.py
@@ -5,9 +5,9 @@ fetchIndex = 1 # Currently fetched index
 committedInsts = 0 # Currently commited instructions
 iq_size = 256
 rob_size = 256
-# Initialize clocks one below their initial time
+# Clock is 2D dict, one dimension is the stage, the other is the instruction 
 clock = {'fe':-1, 'de':0, 're':1, 'di':2, 'is':3, 'wb':4, 'co':5}
-
+time_log = {1:clock} # Initialize clocks one below their initial time for inst 1
     # Stage Queues
 fetchDecode_queue = []
 decodeRename_queue = []
@@ -15,7 +15,7 @@ renameDispatch_queue = []
 issueWB_queue = []
 WBcommit_queue = []
 
-# Instruction queues
+    # Instruction queues
 issue_queue = []
 rob = []
 lsq = []
@@ -57,13 +57,15 @@ for x in range(0,preg_count):
         ready_table['p'+str(x)] = False
 # Fetch stage
 def fetch(fetchIndex): 
+    clock['fe'] += 1
     bundle = data[fetchIndex:1+(issue_width*fetchIndex)]
     for n in bundle:
         fetchDecode_queue.append(n)
-    clock['fe'] += 1
+        clocklog('fe', n[4])
 # Decode stage
 def decode():
     # Move to new queue
+    clock['de'] += 1
     for n in range(0,issue_width):
         inst = fetchDecode_queue.pop(0)
         # Identify producers and consumers based on the instruction type
@@ -81,9 +83,10 @@ def decode():
                 instDict = {'inst':'I', 'read':[inst[1]], 'write':[inst[2]]}
                 decodeRename_queue.append(instDict)
         instDict['pc'] = inst[4]
-    clock['de'] += 1
+        clocklog('de', instDict['pc'])
 
 def rename():
+    clock['re'] += 1
     # Only perform if enough insts to continue, else stall
     if (len(free_list) >=  issue_width):
         for n in range(0,issue_width):
@@ -98,8 +101,10 @@ def rename():
                 map_table[inst['write'][w]] = new_reg
                 inst['write'][w] = map_table[inst['write'][w]]
             renameDispatch_queue.append(inst)
-    clock['re'] += 1
+            clocklog('re', inst['pc'])
+    
 def dispatch():
+    clock['di'] += 1
     for n in range(0,issue_width):
         inst = renameDispatch_queue.pop(0)
         # Add an aging property to the instruction
@@ -112,17 +117,20 @@ def dispatch():
             ready_table[w] = False
         # Send to IQ
         issue_queue.append(inst)
+        clocklog('di', inst['pc'])
         # Send to LSQ if Load/Store
         if (inst['inst'] == 'L') | (inst['inst'] == 'S'):
             lsq.append(inst)
         # Send to ROB, but also track replacement
-        inst_rob = {k:v for k,v in inst.items()}
+        #inst_rob = {k:v for k,v in inst.items()}
+        inst_rob = inst
         for w in inst_rob['write']:
             inst_rob['replaced'] = overwrite_dict[w]
             inst_rob['done'] = False
         rob.append(inst_rob)
-    clock['di'] += 1
-def issue():  
+    
+def issue():
+    clock['is'] += 1
     global issue_queue
     if (len(issue_queue) >= issue_width):
         # Pre-sort by age in ascending order so oldest are always prioritized
@@ -130,7 +138,6 @@ def issue():
         selected = 0 
         imax = len(issue_queue)
         for n in range(0,imax):
-            print(len(issue_queue))
             if (selected >= issue_width): # Stop when you've selected N instructions
                 break
             inst = issue_queue[n-selected]
@@ -141,41 +148,64 @@ def issue():
                     rcheck_flag = 0
             if rcheck_flag:
                 issueWB_queue.append(issue_queue.pop(n-selected))
+                clocklog('is', inst['pc'])
                 selected += 1
         # After instructions have been selected, wake up all the dependent ones
         for s in range(0,selected):
             dep_list = issueWB_queue[-1-s]['write'] 
             for d in dep_list:
                 ready_table[d] = True
-    clock['is'] += 1
+    
 def writeback():
+    clock['wb'] += 1
     if (len(issueWB_queue) >= issue_width):
         for n in range(0,issue_width):
             inst = issueWB_queue.pop(0)
             # Mark instructions as done, dont do this for stores until commit
+            inst['done'] = True
             WBcommit_queue.append(inst)
-    clock['wb'] += 1
+            clocklog('wb', inst['pc'])
+    
 def commit():
-    # Get first inst in ROB that's done
-    for c in range(0,len(rob)):
-        inst = rob[c]
-        committed = 0
-        if inst['done'] == True:
-            # Free the overwritten reg
-            inst = rob.pop(c)
-            free_list.append(inst['replaced'])
-            # Do 
-    clock['co'] += 1 
+    clock['co'] += 1
+    # Get first inst in ROB that's done, since we're not doing branch prediction, just stick to IQ
+    if (len(WBcommit_queue) >= issue_width):
+        inst = WBcommit_queue[0]
+        for n in range(0,issue_width):
+            if inst['done'] == True:
+                inst = WBcommit_queue.pop(0)
+                if inst['inst'] != 'S':
+                    free_list.append(inst['replaced'])
+                clocklog('co', inst['pc'])
+            else:
+                print("This shouldn't happen!")
+    
+# Log clock time for given inst at given stage
+def clocklog(stage, inst):
+    time_log[inst] = clock
+def output_format():
+    print(time_log)
 
 #while (committedInsts < instCount):
-for t in range(0,1):
+for t in range(0,13):
     fetch(fetchIndex)
     decode()
     rename()
     dispatch()
     issue()
-    #commit()
-    print("ROB", rob, '\nLSQ', lsq)
+    writeback()
+    commit()
+    fetchIndex += 1
+output_format()
+    #print(time_log)
+#for t in range(0, instCount):
+ #   for item in time_log.items():
+  #      print(item[1][t])
+   #     if item[0] == 'co':
+    #        print('\n')
+     #   else:
+      #      pass
+        #print("ROB", rob, '\nLSQ', lsq)
 #    print(issue_queue)
 #    print(issueWB_queue)
 #    print(clock)
